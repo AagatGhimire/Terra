e3243b71d23cdd73b2a61f8fb84a1eeff9ffd429
unknown <aagat.gh001@gmail.com>
2023-10-04 18:36:19 +0545

    first version initialized

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:terra/ar_earth_map_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Terra: AR Earth Map',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ArEarthMapScreen(),
+    );
+  }
+}
